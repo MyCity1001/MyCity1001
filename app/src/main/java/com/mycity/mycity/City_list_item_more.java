44ff40c1dd7ff30864538975c5124b1b4a9c2f1c
MyCity <mycity1001@gmail.com>
2019-09-30 20:53:08 +0530

    MyCity 1st commit

---
+++ app/src/main/java/com/mycity/mycity/City_list_item_more.java
@@ -0,0 +1,13 @@
+package com.mycity.mycity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class City_list_item_more extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_list_item_more);
+    }
+}
